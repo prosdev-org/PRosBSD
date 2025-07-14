224ad7bd2dbe705ddc761562980b1f7097831c3b
four4tReS <98309267+akbe2020@users.noreply.github.com>
2025-07-14 22:02:41 +0500

    fix(build.sh): use sudo for docker run command to ensure proper permissions

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build --build-arg HOST_UID=$(id -u) --build-arg HOST_GID=$(id -g) -t pros32-builder .
-docker run --privileged --rm -v "$(pwd):/pros32" -w /pros32 pros32-builder
+sudo docker run --privileged --rm -v "$(pwd):/pros32" -w /pros32 pros32-builder
