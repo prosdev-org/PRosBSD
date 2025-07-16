eab40cbdb7dbe714d196584ce9f2e559e6c9e4bd
four4tReS <98309267+akbe2020@users.noreply.github.com>
2025-07-16 10:31:08 +0500

    refactor(mbr.h): replace #pragma pack with __attribute__((packed))

--- include/fs/mbr.h
+++ include/fs/mbr.h
@@ -1,7 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#pragma pack(push, 1)
 typedef struct {
     uint8_t boot_flag;
     uint8_t chs_start[3];
@@ -9,5 +8,4 @@ typedef struct {
     uint8_t chs_end[3];
     uint32_t lba_start;
     uint32_t num_sectors;
-} mbr_part_t;
-#pragma pack(pop)
+} __attribute__((packed)) mbr_part_t;
