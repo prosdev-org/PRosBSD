d2ad4b5634a6b7d128fdf1c0c22f00e36e4618d6
Han Akbe <han-akbe@prosdev.org>
2025-10-16 20:07:50 +0500

    fix(build): compile options
    Fixes #11

--- cfg/config.cmake
+++ cfg/config.cmake
@@ -4,23 +4,39 @@ set(CMAKE_C_STANDARD 99)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 
 add_compile_options(
-        -O2
-        -c
-        -g
         -m32
+        -march=i386
+        -O2
+        -g
         -fno-builtin
+        -fno-exceptions
+        -fno-rtti
         -fno-stack-protector
         -fno-omit-frame-pointer
-        -fno-inline
-        -fstrength-reduce
+        -fno-strict-aliasing
+        -fno-delete-null-pointer-checks
+        -fwrapv
+        -fno-tree-vrp
+        -fno-reorder-functions
+        -fno-reorder-blocks
+        -fno-pie
         -fno-pic
+        -fno-lto
+        -fno-common
+        -fno-ident
+        -fno-asynchronous-unwind-tables
+        -fno-unwind-tables
         -nostdinc
         -nostdlib
         -Wall
         -Wextra
         -Wshadow
         -Wstrict-aliasing
-        -w
+        -Wpointer-arith
+        -Wcast-align
+        -Wundef
+        -Wwrite-strings
+        -Wmissing-prototypes
 )
 
 set(CMAKE_LINKER_FLAGS -melf_i386)
