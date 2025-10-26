213cd306a5957b9cf182f149b08b904168b2ae2f
Han Akbe <han-akbe@prosdev.org>
2025-10-26 17:41:40 +0500

    refactor(shell): mark shell_loop() as _Noreturn

--- kernel/include/shell/shell.h
+++ kernel/include/shell/shell.h
@@ -1,6 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-void shell_loop();
+_Noreturn void shell_loop();
 
 #endif
--- kernel/shell/shell.c
+++ kernel/shell/shell.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void shell_loop() {
+_Noreturn void shell_loop() {
     for (;;) {
         printf("\033[1;32m~$\033[0m ");
         char str[128];
