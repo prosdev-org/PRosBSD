ba31cec5cea3a62fa72dac11c300153b272aba88
Han Akbe <han-akbe@prosdev.org>
2025-12-15 19:12:05 +0500

    unfeat: remove ArchIoMgr

--- kernel/arch/i386/libarch/include/libarch/io_mgr/vga_text_buffer.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBARCH_IO_MGR_VGA_TEXT_BUFFER_H
-#define LIBARCH_IO_MGR_VGA_TEXT_BUFFER_H
-
-extern ArchIoMgr_mapped_buffer *ArchIoMgrVgaTextBuffer;
-
-#endif
--- kernel/core/src/init.c
+++ kernel/core/src/init.c
@@ -1,10 +1,4 @@
-#include <arch/io_mgr.h>
 _Noreturn void CoreInit_main() {
-    // Demo
-    ArchIoMgr_init();
-    uint16_t buf[80];
-    ArchIoMgr_mapped_buffer_read(ArchIoMgr_mapped_buffer_VGA_TEXT, buf, 80 * sizeof(uint16_t), 80 * sizeof(uint16_t));
-    ArchIoMgr_mapped_buffer_write(ArchIoMgr_mapped_buffer_VGA_TEXT, buf, 80 * sizeof(uint16_t), 0);
     for (;;)
         ;
 }
