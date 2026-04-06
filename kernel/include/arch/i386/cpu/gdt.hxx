eb0b1b2e0a1fdedc2c71e58ec5a17fff4899a42b
Han Akbe <han-akbe@prosdev.org>
2026-04-06 20:31:02 +0500

    refactor(arch/i386/GDT): move macros out of .S

--- kernel/include/arch/i386/cpu/gdt.hxx
+++ kernel/include/arch/i386/cpu/gdt.hxx
@@ -1,8 +1,18 @@
 #ifndef ARCH_I386_GDT_HXX
 #define ARCH_I386_GDT_HXX
 
+#define ARCH_I386_GDT_KERN_CODE_SEG 0x08
+#define ARCH_I386_GDT_KERN_DATA_SEG 0x10
+#define ARCH_I386_GDT_USER_CODE_SEG (0x18 | 3)
+#define ARCH_I386_GDT_USER_DATA_SEG (0x20 | 3)
+#define ARCH_I386_GDT_TSS_SEG       0x28
+
+#ifndef __ASSEMBLER__
+
 namespace Arch::I386::Cpu::Gdt {
     void init();
-}
+} // namespace Arch::I386::Cpu::Gdt
+
+#endif
 
 #endif
