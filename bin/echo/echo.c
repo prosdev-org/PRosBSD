f8b454e4d3cfc49fb4fc16d5773a37af29ca2690
ilnarildarovuch2 <ilnarildarovuch@prosdev.org>
2026-06-04 04:11:28 +0500

    feat(echoers): add echo programs
    (cherry picked from commit 516bd51be4c76bbb0958f760487fdb21c8682c82)

---
+++ bin/echo/echo.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    size_t argsc = 0;
+    for (int i = 1; i < argc; i++) {
+        argsc += strlen(argv[i]) + 1; // terminator
+    }
+
+    char *args = malloc(argsc + 1);
+    if (args == NULL) {
+        fprintf(stderr, "Unable to allocate memory (%s!)\n",
+                strerror(errno));
+        return 1;
+    }
+
+    args[0] = '\0';
+    for (int i = 1; i < argc; i++) {
+        strcat(args, argv[i]);
+        if (argc > i + 1) strcat(args, " ");
+    }
+
+    printf("%s\n", args);
+
+    free(args);
+    return 0;
+}
---
+++ bin/yes/yes.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    size_t argsc = 0;
+    for (int i = 1; i < argc; i++) {
+        argsc += strlen(argv[i]) + 1; // terminator
+    }
+    if (argsc > 1) {
+        char *args = malloc(argsc);
+        if (args == NULL) {
+            fprintf(stderr, "Unable to allocate memory (%s!)\n",
+                    strerror(errno));
+            return 1;
+        }
+
+        args[0] = '\0';
+        for (int i = 1; i < argc; i++) {
+            strcat(args, argv[i]);
+            if (argc > i + 1) strcat(args, " ");
+        }
+        for (;;) {
+            printf("%s\n", args);
+        }
+
+        free(args); // never reached
+    } else {
+        for (;;) {
+            printf("y\n");
+        }
+    }
+    return 0;
+}
